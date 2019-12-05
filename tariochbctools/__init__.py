4ec2b6271fa1eca495bd261307b1862d2228a0e0
Patrick Ruckstuhl <patrick@ch.tario.org>
2019-12-05 23:57:29 +0100

    fixing docstring

--- tariochbctools/__init__.py
+++ tariochbctools/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import get_distribution, DistributionNotFound
-
 """Importers, plugins and price fetchers for beancount."""
+
+from pkg_resources import get_distribution, DistributionNotFound
 
 try:
     dist_name = __name__ 
