fd4b56a26acb873f64acba5305f75fd46d9104ee
Patrick Ruckstuhl <patrick@ch.tario.org>
2019-12-05 23:54:41 +0100

    changed name to tariochbctools

--- tariochbctools/__init__.py
+++ tariochbctools/__init__.py
@@ -4,7 +4,7 @@ from pkg_resources import get_distribution, DistributionNotFound
 """Importers, plugins and price fetchers for beancount."""
 
 try:
-    dist_name = 'tarioch.beancounttools' 
+    dist_name = __name__ 
     __version__ = get_distribution(dist_name).version
 except DistributionNotFound:
     __version__ = 'unknown'
