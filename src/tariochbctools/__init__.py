ca6379e932435fd5f00131326e58a66a931aaa39
Patrick Ruckstuhl <patrick@ch.tario.org>
2019-12-06 23:14:58 +0100

    generated some scaffolding with pyscaffold

--- src/tariochbctools/__init__.py
+++ src/tariochbctools/__init__.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
-"""Importers, plugins and price fetchers for beancount."""
-
 from pkg_resources import get_distribution, DistributionNotFound
 
 try:
-    dist_name = __name__ 
+    # Change here if project is renamed and does not equal the package name
+    dist_name = __name__
     __version__ = get_distribution(dist_name).version
 except DistributionNotFound:
     __version__ = 'unknown'
