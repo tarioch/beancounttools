fca16d87a0fa47ea74969dd7c7de94b697d4813c
Przemator <przemator@gmail.com>
2021-01-04 17:09:21 +0100

    implement Balance entry

--- src/tariochbctools/importers/cembrastatement/importer.py
+++ src/tariochbctools/importers/cembrastatement/importer.py
@@ -1,4 +1,4 @@
-from dateutil.parser import parse
+from datetime import datetime, timedelta
 
 from beancount.ingest import importer
 from beancount.core import data
@@ -7,7 +7,7 @@ from beancount.core.number import D
 from beancount.ingest.importers.mixins import identifier
 
 import camelot
-
+import re
 
 class Importer(identifier.IdentifyMixin, importer.ImporterProtocol):
     """An importer for Cembra Card Statement PDF files."""
@@ -17,6 +17,7 @@ class Importer(identifier.IdentifyMixin, importer.ImporterProtocol):
             ('filename', regexps)
         ])
         self.account = account
+        self.currency = 'CHF'
 
     def file_account(self, file):
         return self.account
@@ -28,13 +29,17 @@ class Importer(identifier.IdentifyMixin, importer.ImporterProtocol):
             date,
             '*',
             '',
-            text.strip(),
+            text,
             data.EMPTY_SET,
             data.EMPTY_SET,
             [
-                data.Posting(self.account, amount.Amount(amt, 'CHF'), None, None, None, None),
+                data.Posting(self.account, amt, None, None, None, None),
             ]
         )
+
+    def createBalanceEntry(self, file, date, amt):
+        meta = data.new_metadata(file.name, 0)
+        return data.Balance(meta, date, self.account, amt, None, None)
 
     def extract(self, file, existing_entries):
         entries = []
@@ -48,21 +53,44 @@ class Importer(identifier.IdentifyMixin, importer.ImporterProtocol):
                 continue
 
             for index, row in df.iterrows():
-                
+
                 trx_date, book_date, text, credit, debit = tuple(row)
-                
+                trx_date, book_date, text, credit, debit = trx_date.strip(), book_date.strip(), text.strip(), credit.strip(), debit.strip()
+
+                # Transaction entry
                 try:
-                    book_date = parse(book_date.strip(), dayfirst=True).date()
+                    book_date = datetime.strptime(book_date, '%d.%m.%Y').date()
                 except (ValueError, OverflowError):
                     book_date = None
 
                 if book_date:
-                    amount = -self.cleanDecimal(debit) if debit else self.cleanDecimal(credit)
+                    amount = self.getAmount(debit, credit)
 
                     if amount:
                         entries.append(self.createEntry(file, book_date, amount, text))
+                    continue
+
+                # Balance entry
+                try:
+                    book_date = re.search('Saldo per (\d\d\.\d\d\.\d\d\d\d) zu unseren Gunsten CHF', text).group(1)
+                    book_date = datetime.strptime(book_date, '%d.%m.%Y').date()
+                    # add 1 day: cembra provides balance at EOD, but beancount checks it at SOD
+                    book_date = book_date + timedelta(days = 1)
+                except:
+                    book_date = None
+
+                if book_date:
+                    amount = self.getAmount(debit, credit)
+
+                    if amount:
+                        entries.append(self.createBalanceEntry(file, book_date, amount))
 
         return entries
 
     def cleanDecimal(self, formattedNumber):
         return D(formattedNumber.replace("'", ""))
+
+    def getAmount(self, debit, credit):
+        amt = -self.cleanDecimal(debit) if debit else self.cleanDecimal(credit)
+        if amt:
+            return amount.Amount(amt, self.currency)
