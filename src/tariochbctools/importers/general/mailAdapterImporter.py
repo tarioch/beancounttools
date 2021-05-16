6e659244d95c334c38e829e036a1b2bb84d38f2e
Patrick Ruckstuhl <patrick@ch.tario.org>
2021-05-16 17:59:23 +0000

    Add Mail Adapter importer

---
+++ src/tariochbctools/importers/general/mailAdapterImporter.py
@@ -0,0 +1,40 @@
+import yaml
+from os import path
+from beancount.ingest import importer, cache
+from imap_tools import MailBox
+import tempfile
+
+
+class MailAdapterImporter(importer.ImporterProtocol):
+    """An importer adapter that fetches file from mails and then calls another importer."""
+
+    def __init__(self, importers):
+        self.importers = importers
+
+    def identify(self, file):
+        return 'mail.yaml' == path.basename(file.name)
+
+    def extract(self, file, existing_entries):
+        config = yaml.safe_load(file.contents())
+
+        with MailBox(config['host']).login(config['user'], config['password'], initial_folder=config['folder']) as mailbox:
+            result = []
+            for msg in mailbox.fetch():
+                processed = False
+                for att in msg.attachments:
+                    with tempfile.TemporaryDirectory() as tmpdirname:
+                        attFileName = path.join(tmpdirname, att.filename)
+                        with open(attFileName, 'wb') as attFile:
+                            attFile.write(att.payload)
+                            fileMemo = cache.get_file(attFileName)
+
+                            for delegate in self.importers:
+                                if delegate.identify(fileMemo):
+                                    newEntries = delegate.extract(fileMemo, existing_entries)
+                                    result.extend(newEntries)
+                                    processed = True
+
+                if processed and 'targetFolder' in config:
+                    mailbox.move(msg.uid, config['targetFolder'])
+
+        return result
