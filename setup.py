ea12dcf14053f3d03919d6fc1006750642e3d421
Patrick Ruckstuhl <patrick@ch.tario.org>
2021-09-10 19:34:29 +0000

    setup correct local_scheme for testpypi

--- setup.py
+++ setup.py
@@ -10,7 +10,12 @@ from setuptools import setup
 
 if __name__ == "__main__":
     try:
-        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+        setup(
+            use_scm_version={
+                "version_scheme": "no-guess-dev",
+                "local_scheme": "no-local-version",
+            }
+        )
     except:  # noqa
         print(
             "\n\nAn error occurred while building the project, "
