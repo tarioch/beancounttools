fcbc9cc7bd779d415d95aa8b65d9c878e60ab09b
Patrick Ruckstuhl <patrick@ch.tario.org>
2021-09-10 18:06:36 +0000

    Use tox

--- src/tariochbctools/__init__.py
+++ src/tariochbctools/__init__.py
@@ -8,7 +8,7 @@ else:
 
 try:
     # Change here if project is renamed and does not equal the package name
-    dist_name = "tariochbctools"
+    dist_name = __name__
     __version__ = version(dist_name)
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
