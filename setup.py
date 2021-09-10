1d3f6eb851e28119b343d0e8044f18c241eb743a
Patrick Ruckstuhl <patrick@ch.tario.org>
2021-09-10 11:22:17 +0000

    upgrade to latest pyscaffold

--- setup.py
+++ setup.py
@@ -1,27 +1,21 @@
-# -*- coding: utf-8 -*-
 """
-    Setup file for beancounttools.
+    Setup file for tariochbctools.
     Use setup.cfg to configure your project.
 
-    This file was generated with PyScaffold 3.2.3.
+    This file was generated with PyScaffold 4.0.2.
     PyScaffold helps you to put up the scaffold of your new Python project.
     Learn more under: https://pyscaffold.org/
 """
-import sys
-
-from pkg_resources import VersionConflict, require
 from setuptools import setup
 
-try:
-    require('setuptools>=38.3')
-except VersionConflict:
-    print("Error: version of setuptools is too old (<38.3)!")
-    sys.exit(1)
-
-
-def local_scheme(version):
-    return ''
-
-
 if __name__ == "__main__":
-    setup(use_pyscaffold={"local_scheme": local_scheme})
+    try:
+        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+    except:  # noqa
+        print(
+            "\n\nAn error occurred while building the project, "
+            "please ensure you have the most updated version of setuptools, "
+            "setuptools_scm and wheel with:\n"
+            "   pip install -U setuptools setuptools_scm wheel\n\n"
+        )
+        raise
