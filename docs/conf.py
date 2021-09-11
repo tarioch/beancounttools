999064841c4eeb1dac3f95055e2510e1bfcdbfce
Patrick Ruckstuhl <patrick@ch.tario.org>
2021-09-11 11:03:34 +0000

    use rdt theme

--- docs/conf.py
+++ docs/conf.py
@@ -78,6 +78,7 @@ extensions = [
     "sphinx.ext.ifconfig",
     "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -147,15 +148,12 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    "sidebar_width": "300px",
-    "page_width": "1200px"
-}
+html_theme_options = {"sidebar_width": "300px", "page_width": "1200px"}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
@@ -245,7 +243,13 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-    ("index", "user_guide.tex", "tariochbctools Documentation", "Patrick Ruckstuhl", "manual")
+    (
+        "index",
+        "user_guide.tex",
+        "tariochbctools Documentation",
+        "Patrick Ruckstuhl",
+        "manual",
+    )
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
