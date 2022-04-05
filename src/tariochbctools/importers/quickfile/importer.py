fa0e0c2456d71544e9091fa014c3188c8e7ade64
Mark Ferry <mark@markferry.net>
2022-04-06 00:37:14 +0100

    quickfile: QuickFile bank search

--- src/tariochbctools/importers/quickfile/importer.py
+++ src/tariochbctools/importers/quickfile/importer.py
@@ -1,14 +1,16 @@
+import logging
+import uuid
 from datetime import datetime
+from hashlib import md5
 from os import path
-from typing import NamedTuple
+from typing import Dict, List, NamedTuple
 
+import requests
 import yaml
 from beancount.core import amount, data
 from beancount.core.number import D
 from beancount.ingest import importer
 from undictify import type_checked_constructor
-
-# https://api.quickfile.co.uk/method/bank
 
 
 @type_checked_constructor(skip=True, convert=True)
@@ -17,9 +19,9 @@ class QuickFileTransaction(NamedTuple):
 
     TransactionDate: str
     Reference: str
-    Amount: float
+    Amount: str  # otherwise tiny conversion losses
     TagStatus: str
-    TransactionId: int
+    TransactionId: str
 
     def to_beancount_transaction(self, local_account, currency, invert_sign=False):
         tx_amount = D(self.Amount)
@@ -31,7 +33,7 @@ class QuickFileTransaction(NamedTuple):
         }
 
         meta = data.new_metadata("", 0, metakv)
-        date = datetime.fromisoformat(self.TransactionDate)
+        date = datetime.fromisoformat(self.TransactionDate).date()
 
         entry = data.Transaction(
             meta,
@@ -55,16 +57,112 @@ class QuickFileTransaction(NamedTuple):
         return entry
 
 
+@type_checked_constructor(skip=True, convert=True)
+class QuickFileResponseMetaData(NamedTuple):
+    RecordsetCount: int
+    ReturnCount: int
+    BankName: str
+    BankType: str
+    AccountNo: str
+    SortCode: str
+    Currency: str
+    CurrentBalance: str
+
+
+@type_checked_constructor(skip=True, convert=True)
+class QuickFileBankSearch(NamedTuple):
+    MetaData: QuickFileResponseMetaData
+    Transactions: Dict[str, List[QuickFileTransaction]]
+
+
+class QuickFile:
+    """Encapsulate QuickFile API protocol and data types"""
+
+    DOMAIN = "quickfile.co.uk"
+    API_VERSION_SLUG = "1_2"
+
+    def __init__(self, account_number, api_key, app_id):
+        self.account_number = account_number
+        self.api_key = api_key
+        self.app_id = app_id
+        self._update_submission_number()
+
+    @staticmethod
+    def auth_md5(account_number, api_key, submission_number):
+        md5_str = (account_number + api_key + str(submission_number)).encode("utf-8")
+
+        return md5(md5_str).hexdigest()
+
+    def request_header(self):
+        auth_md5 = self.auth_md5(
+            self.account_number, self.api_key, self.submission_number
+        )
+
+        header = {
+            "MessageType": "Request",
+            "SubmissionNumber": str(self.submission_number),
+            "Authentication": {
+                "AccNumber": str(self.account_number),
+                "MD5Value": auth_md5,
+                "ApplicationID": str(self.app_id),
+            },
+        }
+        return header
+
+    def _update_submission_number(self):
+        self.submission_number = uuid.uuid4()
+
+    def _post(self, endpoint, endpoint_data):
+        header = self.request_header()
+        post_data = {"payload": {"Header": header, "Body": endpoint_data}}
+
+        r = requests.post(
+            f"https://api.{self.DOMAIN}/{self.API_VERSION_SLUG}/{endpoint}",
+            json=post_data,
+        )
+
+        if not r:
+            try:
+                r.raise_for_status()
+            except requests.HTTPError as e:
+                logging.warning(e)
+
+        self._update_submission_number()
+
+        return r
+
+    def bank_search(self, account_number):
+        endpoint_data = {
+            "SearchParameters": {
+                "ReturnCount": "10",
+                "Offset": "0",
+                "OrderResultsBy": "TransactionDate",
+                "OrderDirection": "ASC",
+                "NominalCode": str(account_number),
+            }
+        }
+
+        response = self._post("bank/search", endpoint_data).json()
+        body = response["Bank_Search"]["Body"]
+        return QuickFileBankSearch(**body)
+
+
 class Importer(importer.ImporterProtocol):
     """An importer for QuickFile"""
 
     def __init__(self):
+        self.quickfile = None
         self.config = None
         self.existing_entries = None
 
     def _configure(self, file, existing_entries):
         with open(file.name, "r") as config_file:
             self.config = yaml.safe_load(config_file)
+            self.quickfile = QuickFile(
+                account_number=self.config["account_number"],
+                api_key=self.config["api_key"],
+                app_id=self.config["app_id"],
+            )
         self.existing_entries = existing_entries
 
     def identify(self, file):
@@ -75,29 +173,37 @@ class Importer(importer.ImporterProtocol):
 
     def extract(self, file, existing_entries=None):
         self._configure(file, existing_entries)
-        headers = {}
         entries = []
-        entries.extend(self._extract_bank_transactions("account_one", headers))
+
+        for bank_account in self.config["accounts"].keys():
+            entries.extend(self._extract_bank_transactions(bank_account))
+
         return entries
 
-    def _extract_bank_transactions(self, bank_account, headers, invert_sign=False):
+    def _extract_bank_transactions(self, bank_account, invert_sign=False):
         entries = []
-        response = {}
-        transactions = response
+        response = self.quickfile.bank_search(bank_account)
+        metadata = response.MetaData
+        transactions = response.Transactions["Transaction"]
+        local_account = self.config["accounts"].get(bank_account)
 
-        for trx in response:
+        for trx in transactions:
             entries.extend(
-                self._extract_transaction(trx, bank_account, transactions, invert_sign)
+                self._extract_transaction(
+                    trx, local_account, metadata, transactions, invert_sign
+                )
             )
 
         return entries
 
-    def _extract_transaction(self, trx, local_account, transactions, invert_sign):
+    def _extract_transaction(
+        self, trx, local_account, metadata, transactions, invert_sign
+    ):
         entries = []
-        currency = ""  # TODO: extract from metadata
 
-        t = QuickFileTransaction(**trx)
-        entry = t.to_beancount_transaction(local_account, currency, invert_sign)
+        entry = trx.to_beancount_transaction(
+            local_account, metadata.Currency, invert_sign
+        )
         entries.append(entry)
 
         return entries
--- tests/tariochbctools/importers/test_quickfile.py
+++ tests/tariochbctools/importers/test_quickfile.py
@@ -1,7 +1,9 @@
 import json
+from unittest.mock import MagicMock, call
 
 import pytest
-from beancount.core.amount import Decimal as D
+from beancount.core import data
+from beancount.core.number import D
 from beancount.ingest import cache
 
 from tariochbctools.importers.quickfile import importer as qfimp
@@ -9,6 +11,12 @@ from tariochbctools.importers.quickfile import importer as qfimp
 # pylint: disable=protected-access
 
 TEST_CONFIG = b"""
+account_number: "YOUR_ACCOUNT_NUMBER"
+api_key: SOME_API_KEY
+app_id: SOME_APP_ID
+accounts:
+    1200: Assets:Other
+    1201: Assets:Savings
 """
 
 # Example from the quickfile API docs
@@ -19,6 +27,53 @@ TEST_TRX = b"""
     "Amount": -4.75,
     "TagStatus": null,
     "TransactionId": 666
+}
+"""
+
+TEST_META_DATA = b"""
+{
+    "RecordsetCount": 6853,
+    "ReturnCount": 1,
+    "BankName": "Current Account",
+    "BankType": "CURRENT",
+    "AccountNo": "90145741",
+    "SortCode": "208246",
+    "Currency": "GBP",
+    "CurrentBalance": 3164.97
+}
+"""
+
+TEST_BANK_SEARCH = b"""
+{
+  "Bank_Search": {
+    "Header": {
+      "MessageType": "Response",
+      "SubmissionNumber": "8ebba60a-95ce-44af-a8c7-e827ac1ae117"
+    },
+    "Body": {
+      "MetaData": {
+        "RecordsetCount": 6853,
+        "ReturnCount": 1,
+        "BankName": "Current Account",
+        "BankType": "CURRENT",
+        "AccountNo": "90145741",
+        "SortCode": "208246",
+        "Currency": "GBP",
+        "CurrentBalance": 3164.97
+      },
+      "Transactions": {
+        "Transaction": [
+          {
+            "TransactionDate": "2017-01-04T00:00:00",
+            "Reference": "WATERSTONES REF 364 2177333010 BCC",
+            "Amount": -4.75,
+            "TagStatus": null,
+            "TransactionId": 0
+          }
+        ]
+      }
+    }
+  }
 }
 """
 
@@ -55,15 +110,68 @@ def quickfile_importer_factory(tmp_path):
 
 @pytest.fixture(name="tmp_trx")
 def tmp_trx_fixture():
-    yield json.loads(TEST_TRX)
+    j = json.loads(TEST_TRX)
+    yield qfimp.QuickFileTransaction(**j)
+
+
+@pytest.fixture(name="tmp_metadata")
+def tmp_metadata_fixture():
+    j = json.loads(TEST_META_DATA)
+    metadata = qfimp.QuickFileResponseMetaData(**j)
+    yield metadata
 
 
 def test_identify(importer, tmp_config):
     assert importer.identify(tmp_config)
 
 
-def test_extract_transaction_simple(importer, tmp_trx):
+def test_extract_transaction_simple(importer, tmp_trx, tmp_metadata):
     entries = importer._extract_transaction(
-        tmp_trx, "Assets:Other", [tmp_trx], invert_sign=False
+        tmp_trx, "Assets:Other", tmp_metadata, [tmp_trx], invert_sign=False
     )
-    assert entries[0].postings[0].units.number == D(str(tmp_trx["Amount"]))
+    data.sanity_check_types(entries[0])
+
+
+def test_extract_transaction_invert_sign(importer, tmp_trx, tmp_metadata):
+    """Show that sign inversion works"""
+    entries = importer._extract_transaction(
+        tmp_trx, "Assets:Other", tmp_metadata, [tmp_trx], invert_sign=True
+    )
+    assert entries[0].postings[0].units.number == -D(str(tmp_trx.Amount))
+
+
+def test_extract_transaction_has_quickfile_id(importer, tmp_trx, tmp_metadata):
+    """Ensure mandatory IDs are in extracted transactions."""
+    entries = importer._extract_transaction(
+        tmp_trx, "Assets:Other", tmp_metadata, [tmp_trx], invert_sign=False
+    )
+    assert "quickfile_id" in entries[0].meta
+
+
+def test_request_header_auth(importer, tmp_config):
+    header = importer.quickfile.request_header()
+    auth = header.get("Authentication")
+
+    config = importer.config
+
+    assert auth["AccNumber"] == config["account_number"]
+    assert auth["MD5Value"] == importer.quickfile.auth_md5(
+        config["account_number"],
+        config["api_key"],
+        importer.quickfile.submission_number,
+    )
+    assert auth["ApplicationID"] == config["app_id"]
+
+
+def test_bank_search():
+    response = json.loads(TEST_BANK_SEARCH)
+    body = response["Bank_Search"]["Body"]
+    bs = qfimp.QuickFileBankSearch(**body)
+    assert len(bs.Transactions["Transaction"]) == 1
+
+
+def test_extract_all_accounts(importer, tmp_config):
+    importer._extract_bank_transactions = MagicMock()
+    importer.extract(tmp_config)
+    calls = [call(1200), call(1201)]
+    importer._extract_bank_transactions.assert_has_calls(calls, any_order=True)
