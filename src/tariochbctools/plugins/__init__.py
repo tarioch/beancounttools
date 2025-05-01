5ade8639ba75bba2518875852f4c1c45c5ade4d3
Patrick Ruckstuhl <patrick@ch.tario.org>
2025-05-01 21:34:41 +0000

    upgrade precommit checks

--- src/tariochbctools/plugins/__init__.py
+++ src/tariochbctools/plugins/__init__.py
@@ -1,2 +1,1 @@
-"""Beancount plugins.
-"""
+"""Beancount plugins."""
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
 """
-    Dummy conftest.py for tariochbctools.
+Dummy conftest.py for tariochbctools.
 
-    If you don't know what this is for, just leave it empty.
-    Read more about conftest.py under:
-    - https://docs.pytest.org/en/stable/fixture.html
-    - https://docs.pytest.org/en/stable/writing_plugins.html
+If you don't know what this is for, just leave it empty.
+Read more about conftest.py under:
+- https://docs.pytest.org/en/stable/fixture.html
+- https://docs.pytest.org/en/stable/writing_plugins.html
 """
 
 # import pytest
